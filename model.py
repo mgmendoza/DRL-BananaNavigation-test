79c774b64b7c32725815f10ac5429c077c335f7b
mgmendoza <mgmendoza@alumni.usc.edu>
2020-12-30 10:09:34 -0800

    working version for the banana collector

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 class QNetwork(nn.Module):
     """Actor (Policy) Model."""
 
-    def __init__(self, state_size, action_size, seed, fc1_units=128, fc2_units=128):
+    def __init__(self, state_size, action_size, seed, fc1_units=256, fc2_units=128):
         """Initialize parameters and build model.
         Params
         ======
